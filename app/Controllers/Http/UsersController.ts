4e42b81f7d4b0917f17946b4eeaf67d647ba49b5
lucasdev-96 <lucasgodoidev96@gmail.com>
2022-07-14 02:49:17 -0300

    feat: :sparkles: Criando validators

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,10 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
+import CreateUserValidator from 'App/Validators/CreateUserValidator'
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
-    const userPayload = request.only(['username', 'password', 'email', 'avatar'])
+    const userPayload = await request.validate(CreateUserValidator)
     const userResponse = await User.create(userPayload)
     return response.created({ userResponse })
   }
